6f5dd1d0751ba9dc27620578087d9edd5c33b479
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-02-17 17:18:20 +0530

    update

---
+++ robinhood/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+     {/* Header */}
+     <div className="app_header">
+      <Header/>
+     </div>
+     <div className="app_body">
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ robinhood/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header_wrapper">
+      {/* logo */}
+      <div className="header_logo">
+        TEST
+      </div>
+      {/* search */}
+      {/* menu item */}
+    </div>
+  );
+}
+
+export default Header;
